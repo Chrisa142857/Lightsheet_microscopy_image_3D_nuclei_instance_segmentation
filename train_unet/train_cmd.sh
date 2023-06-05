0cfbf99950183d643f7781ce8e9843971631c151
Chrisa142857 <wzq10101@gmail.com>
2023-06-04 22:12:48 -0400

    Trainval 2D Unet commands

--- train_unet/train_cmd.sh
+++ train_unet/train_cmd.sh
@@ -1,3 +1,3 @@
 NOW="$(date +"%m-%d-%Y-%T")"
-python -m cellpose --use_gpu --gpu_device 1 --train --dir /ram/USERS/ziquanw/cellpose_exp/data_P4_P15/train --test_dir /ram/USERS/ziquanw/cellpose_exp/data_P4_P15/val --save_each --n_epochs 100 --save_every 10 --verbose >> train_log_$NOW.txt
+python -m cellpose/cellpose --use_gpu --gpu_device 1 --train --dir ../downloads/train_data/data_P4_P15/train --test_dir ../downloads/train_data/data_P4_P15/val --save_each --n_epochs 100 --save_every 10 --verbose >> train_log_$NOW.txt
 
