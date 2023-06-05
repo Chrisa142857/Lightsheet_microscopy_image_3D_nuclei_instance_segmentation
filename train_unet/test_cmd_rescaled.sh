18b75016076ae5d7b6174573cbedd6ab2f90b42d
Chrisa142857 <wzq10101@gmail.com>
2023-06-04 22:12:48 -0400

    Trainval 2D Unet commands

--- train_unet/test_cmd_rescaled.sh
+++ train_unet/test_cmd_rescaled.sh
@@ -1,13 +1,13 @@
 NOW="$(date +"%m-%d-%Y-%T")"
 
 IMG_PATH="../data/Felix_P4/Felix_01x00_C00_Z0800_sub5_2.tif"
-python -m cellpose --use_gpu --gpu_device 1 --image_path $IMG_PATH --pretrained_model /ram/USERS/ziquanw/cellpose_exp/data_P4_P15_rescaled-as-P15/train/models/cellpose_residual_on_style_on_concatenation_off_train_2023_05_15_12_23_32.710910_epoch_81 --do_3D --save_tif --no_npy --verbose --savedir /ram/USERS/ziquanw/cellpose_exp/test_out_rescaled >> test_log_$NOW.txt
+python -m cellpose/cellpose --use_gpu --gpu_device 1 --image_path $IMG_PATH --pretrained_model ../downloads/train_data/data_P4_P15_rescaled-as-P15/train/models/cellpose_residual_on_style_on_concatenation_off_train_2023_05_15_12_23_32.710910_epoch_81 --do_3D --save_tif --no_npy --verbose --savedir ../downloads/train_data/test_out_rescaled >> test_log_$NOW.txt
 
 
 IMG_PATH="../data/Carolyn_org_Sept/images/Side0[00x02]sub4.tif"
-python -m cellpose --use_gpu --gpu_device 1 --image_path $IMG_PATH --pretrained_model /ram/USERS/ziquanw/cellpose_exp/data_P4_P15_rescaled-as-P15/train/models/cellpose_residual_on_style_on_concatenation_off_train_2023_05_15_12_23_32.710910_epoch_81 --do_3D --save_tif --no_npy --verbose --savedir /ram/USERS/ziquanw/cellpose_exp/test_out_rescaled >> test_log_$NOW.txt
+python -m cellpose/cellpose --use_gpu --gpu_device 1 --image_path $IMG_PATH --pretrained_model ../downloads/train_data/data_P4_P15_rescaled-as-P15/train/models/cellpose_residual_on_style_on_concatenation_off_train_2023_05_15_12_23_32.710910_epoch_81 --do_3D --save_tif --no_npy --verbose --savedir ../downloads/train_data/test_out_rescaled >> test_log_$NOW.txt
 
 
 IMG_PATH="../data/Carolyn_org_Sept/images/Side0[01x00]sub1.tif"
-python -m cellpose --use_gpu --gpu_device 1 --image_path $IMG_PATH --pretrained_model /ram/USERS/ziquanw/cellpose_exp/data_P4_P15_rescaled-as-P15/train/models/cellpose_residual_on_style_on_concatenation_off_train_2023_05_15_12_23_32.710910_epoch_81 --do_3D --save_tif --no_npy --verbose --savedir /ram/USERS/ziquanw/cellpose_exp/test_out_rescaled >> test_log_$NOW.txt
+python -m cellpose/cellpose --use_gpu --gpu_device 1 --image_path $IMG_PATH --pretrained_model ../downloads/train_data/data_P4_P15_rescaled-as-P15/train/models/cellpose_residual_on_style_on_concatenation_off_train_2023_05_15_12_23_32.710910_epoch_81 --do_3D --save_tif --no_npy --verbose --savedir ../downloads/train_data/test_out_rescaled >> test_log_$NOW.txt
 
--- train_unet/train_cmd_rescaled.sh
+++ train_unet/train_cmd_rescaled.sh
@@ -1,5 +1,5 @@
 NOW="$(date +"%m-%d-%Y-%T")"
-# python -m cellpose --use_gpu --gpu_device 0 --train --dir /ram/USERS/ziquanw/cellpose_exp/data_P4_P15_rescaled-as-P15/train --test_dir /ram/USERS/ziquanw/cellpose_exp/data_P4_P15_rescaled-as-P15/val --save_each --n_epochs 100 --save_every 10 --verbose >> train_log_$NOW.txt
+# python -m cellpose/cellpose --use_gpu --gpu_device 0 --train --dir ../downloads/train_data/data_P4_P15_rescaled-as-P15/train --test_dir ../downloads/train_data/data_P4_P15_rescaled-as-P15/val --save_each --n_epochs 100 --save_every 10 --verbose >> train_log_$NOW.txt
 
-python -m cellpose --use_gpu --gpu_device 0 --train --pretrained_model nuclei --dir /ram/USERS/ziquanw/cellpose_exp/data_P4_P15_rescaled-as-P15/train --test_dir /ram/USERS/ziquanw/cellpose_exp/data_P4_P15_rescaled-as-P15/val --save_each --n_epochs 100 --save_every 10 --verbose >> train_log_$NOW.txt
+python -m cellpose/cellpose --use_gpu --gpu_device 0 --train --pretrained_model nuclei --dir ../downloads/train_data/data_P4_P15_rescaled-as-P15/train --test_dir ../downloads/train_data/data_P4_P15_rescaled-as-P15/val --save_each --n_epochs 100 --save_every 10 --verbose >> train_log_$NOW.txt
 
