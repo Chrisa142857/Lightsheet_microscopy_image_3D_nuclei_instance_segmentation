cb702ea5aab40065c132e968ebe964b300bc7f72
Chrisa142857 <wzq10101@gmail.com>
2023-06-18 05:16:37 -0400

    Avoid out of memory

--- run_whole_brain/run_stitch_step.py
+++ run_whole_brain/run_stitch_step.py
@@ -1,4 +1,4 @@
-import h5py, torch, os, json
+import h5py, torch, os, json, signal
 from tqdm import trange, tqdm
 import numpy as np
 from torch_geometric.data import Data
@@ -18,14 +18,12 @@ pair_tag = 'pair15'
 r = '/lichtman/ziquanw/Lightsheet/results/P4/%s/%s' % (pair_tag, brain_tag)
 img_r = '/lichtman/Felix/Lightsheet/P4/%s/output_%s/stitched' % (pair_tag, brain_tag)
 brain_result_path = '%s/%s_NIS_results.h5' % (r, brain_tag)
-# brain_image_path = 'wholebrain_results/P4_weights/%s_images.h5' % brain_tag
-# brain_flow_path = 'wholebrain_results/P4_weights/%s_flow.h5' % brain_tag
 brain_flow_dir = '%s/flow_3d' % r
-# out_result_path = 'wholebrain_results/P4_weights/%s_stitched.h5' % brain_tag
 remap_save_path = '%s/%s_remap.json' % (r, brain_tag)
 device = 'cuda:1' # 'cuda:1'
 
 def main():    
+    print(datetime.datetime.now(), "Start python program f{sys.argv}", flush=True)
     max_nuclei_size = (10, 30, 30) # 
     graph_model = StitchModel(device)
     graph_model.to(device)
@@ -40,15 +38,14 @@ def main():
     next_id, dims = match_stitch_pt(stitch_pts) # only stitch pre slice to the next
     N = stitch_pts.shape[0]
     next_id = [None] + [stitch_pts[i][0]+1 for i in range(1, N-1)] + [None]
-    # total = len(next_id[next_id!=None])
     total = len(dims[(next_id!=None) & (dims==0)])
-    stitching_i = 0
+    stitching_i = 8
     # if os.path.exists(remap_save_path):
     #     with open(remap_save_path, 'r') as jsonf:
     #         out_json = json.load(jsonf)
     # else:
     out_json = []
-    for i in range(N):
+    for i in range(9, N):
         if next_id[i] is None: continue
         if dims[i] != 0: continue
         loader_pool = Pool(processes=2)
@@ -61,28 +58,28 @@ def main():
         next_slice_id = [next_id[i]]
         # next_slice_id = [next_slice_id.item()+j for j in range(max_nuclei_size[dim//2])]
         indices = [pre_slice_id] + next_slice_id
-        print(datetime.datetime.now(), "[%03d/%d] Start stitch slices %s" % (stitching_i, total, indices))
+        print(datetime.datetime.now(), "[%03d/%d] Start stitch slices %s" % (stitching_i, total, indices), flush=True)
         zs, ze, ys, ye, xs, xe = pt # ~s and ~e are the same in the dim of stitching plane, e.g., zs = ze = 64
-        print(datetime.datetime.now(), "Load mask, image, and nuclei probability flow")
+        print(datetime.datetime.now(), "Load mask, image, and nuclei probability flow", flush=True)
         assert dim == 0, "Not implemented"
         slice = seg[indices, ys:ye, xs:xe]
         pre_img_fn = flow_fnlist[pre_slice_id].split('_resample')[0]+'.tif'
         next_img_fn = flow_fnlist[next_slice_id[0]].split('_resample')[0]+'.tif'
         image = list(loader_pool.imap(utils.imread, [os.path.join(img_r, pre_img_fn), os.path.join(img_r, next_img_fn)]))
+        get_rid_of_zoombie_process(loader_pool)
         image = np.stack([np.asarray(img) for img in image])
-        flow = load_flow(brain_flow_dir, pre_slice_id, next_slice_id[0], ys, ye, xs, xe, loader_pool)
+        loader_pool = Pool(processes=2)
+        flow = load_flow_multiprocess(brain_flow_dir, pre_slice_id, next_slice_id[0], ys, ye, xs, xe, loader_pool)
+        get_rid_of_zoombie_process(loader_pool)
+        print(datetime.datetime.now(), "Done loading", flush=True)
         dim_vol= dim // 2
         pre_mask, next_stack = np.take(slice, 0, axis=dim_vol), np.take(slice, [ii for ii in range(1, len(indices))], axis=dim_vol)
         pre_slice_image, next_slice_image = np.take(image, 0, axis=dim_vol), np.take(image, 1, axis=dim_vol)
         pre_slice_image, next_slice_image = norm_img(pre_slice_image), norm_img(next_slice_image)
         pre_flow, next_flow = np.take(flow, 0, axis=dim_vol+1), np.take(flow, 1, axis=dim_vol+1)
         remap_dict = stitch_one_gap(graph_model, pre_mask, next_stack, pre_slice_image, next_slice_image, pre_flow, next_flow, dim_vol, graph_node_pool, graph_edge_pool)
-        loader_pool.close()
-        graph_edge_pool.close()
-        graph_node_pool.close()
-        loader_pool.join()
-        graph_edge_pool.join()
-        graph_node_pool.join()
+        get_rid_of_zoombie_process(graph_edge_pool)
+        get_rid_of_zoombie_process(graph_node_pool)
         if remap_dict is None: continue
         out_json.append({
             'stitching_slices': [pre_slice_id, next_slice_id[0]],
@@ -97,33 +94,37 @@ def main():
 
 def stitch_one_gap(model, pre_mask, next_stack, pre_slice, next_slice, pre_flow, next_flow, axis, graph_node_pool, graph_edge_pool):
     next_mask = np.take(next_stack, 0, axis=axis)
-    print(datetime.datetime.now(), 'Build graph of nuclei between two slices')
+    print(datetime.datetime.now(), 'Build graph of nuclei between two slices', flush=True)
     input, orig_remaps = build_graph(pre_slice, next_slice, pre_mask, next_mask, pre_flow, next_flow, model.preprocess, graph_node_pool, graph_edge_pool)
     if input is None: 
-        print(datetime.datetime.now(), 'Skip, no nuclei in one slice')
+        print(datetime.datetime.now(), 'Skip, no nuclei in one slice', flush=True)
         return None
-    print(datetime.datetime.now(), 'Run stitching model')
-    try:
-        edge_pred = model(input)
-    except:
-        print(datetime.datetime.now(), 'Graph model out of cuda mem, using CPU')
-        model = model.cpu()
-        edge_pred = model(input.cpu())
-        model = model.to(device)
-    print(datetime.datetime.now(), 'Decode the output as a ID remap dict')
-    pred = edge_pred.argmax(1).detach().cpu()
-    is_new_cell = pred == 0
+    print(datetime.datetime.now(), 'Run stitching model', flush=True)
+    with torch.no_grad():
+        try:
+            edge_pred = model(input)
+            pred = edge_pred.argmax(1).detach().cpu()
+            
+        except:
+            print(datetime.datetime.now(), 'Graph model out of CUDA mem, using CPU', flush=True)
+            model = model.cpu()
+            edge_pred = model(input.cpu())
+            model = model.to(device)
+            pred = edge_pred.argmax(1)
+    print(datetime.datetime.now(), 'Decode the output as a ID remap dict', flush=True)
     edge_index = input.edge_index.detach().cpu()
+    is_new_cell = pred == 0
     node0 = edge_index[0].unique() # N0
     edge_index[1] = edge_index[1] - edge_index[0].max() - 1 # recover mask1 node id
     node1 = torch.stack([edge_index[1, edge_index[0]==nid] for nid in node0]) # N1
     node1 = torch.index_select(node1[~is_new_cell], 1, pred[~is_new_cell]-1).diag()#.detach().cpu() # N1
     node0 = node0[~is_new_cell]
+    edge_pred = edge_pred[~is_new_cell]
     ## get new id of next stack
     oldid2newid = {}
     for nid in node1.unique():
         nid_place = node1==nid
-        scores = edge_pred[~is_new_cell][nid_place].max(1)[0]
+        scores = edge_pred[nid_place].max(1)[0]
         oldid2newid[nid.item()] = (node0[nid_place][scores.argmax()]).item()
     ## fastremap made the id in mask starts from 1, while node id starts from 0 in the graph
     oldid2newid = {k+1:v+1 for k,v in oldid2newid.items()}
@@ -134,8 +135,17 @@ def stitch_one_gap(model, pre_mask, next_stack, pre_slice, next_slice, pre_flow,
     for k, v in oldid2newid.items():
         new_remap[orig_remaps[1][k]] = orig_remaps[0][v]
 
-    print(datetime.datetime.now(), 'Complete, there are %d nuclei IDs in the chunk needs to be changed' % len(new_remap))
+    print(datetime.datetime.now(), 'Complete, there are %d nuclei IDs in the chunk needs to be changed' % len(new_remap), flush=True)
     return new_remap
+
+
+def get_rid_of_zoombie_process(pool):
+    processes = pool._pool[:]
+    for _curr_process in processes:
+        pid = _curr_process.pid
+        _curr_process.terminate()
+        pool._pool.remove(_curr_process)
+        # print(datetime.datetime.now(), f'Kill child process {pid}', flush=True)
 
 
 def norm_img(img):
@@ -216,11 +226,9 @@ def sort_fs(fs, get_i):
     return out
 
 
-def load_flow(fdir, zmin, zmax, ymin, ymax, xmin, xmax, pool):
+def load_flow_multiprocess(fdir, zmin, zmax, ymin, ymax, xmin, xmax, pool):
     flist = sort_fs([fn for fn in os.listdir(fdir) if fn.endswith('.npy')], get_i_xy)
     fs = [fn for fn in flist if zmin <= get_i_xy(fn) < zmax+1]
-    # with Pool(processes=4) as pool:
-        # data = list(tqdm(pool.imap(load_data, [(os.path.join(fdir, fn), ymin, ymax, xmin, xmax) for fn in fs]), total=len(fs), desc="Load flow")) # [3 x Y x X]
     data = list(pool.imap(load_data, [(os.path.join(fdir, fn), ymin, ymax, xmin, xmax) for fn in fs])) # [3 x Y x X]
     return np.stack(data, axis=1)[:3]
 
